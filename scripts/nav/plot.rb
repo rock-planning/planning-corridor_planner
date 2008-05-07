658a51f08ca00fe4ec602a78e436cfeccc876b5e
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2008-05-07 17:15:15 +0200

    [nav] allow plotting the cost image

--- scripts/nav/plot.rb
+++ scripts/nav/plot.rb
@@ -11,18 +11,23 @@ class Plotter
         @t = Tioga::FigureMaker.default
 
         @data = io.read
-        t.def_figure("Paths") { plot_dstar_result }
-        t.def_figure("Costs") { plot_dstar_costs }
+        t.def_figure("Graph") { dstar_graph }
+        t.def_figure("Costs") { dstar_costs }
     end
 
-    def plot_dstar_result
+    def read_header(io)
+        values = io.readline.split(" ")
+        x_size, y_size = Float(values[0]), Float(values[1])
+        values = io.readline.split(" ")
+        x_start, y_start, x_goal, y_goal = Float(values[0]), Float(values[1]), Float(values[2]), Float(values[3])
+        return x_size, y_size, x_start, y_start, x_goal, y_goal
+    end
+
+    def dstar_graph
         t.line_width = 0
 
         file = StringIO.new data
-        values = file.readline.split(" ")
-        x_size, y_size = Float(values[0]), Float(values[1])
-        values = file.readline.split(" ")
-        x_start, y_start, x_goal, y_goal = Float(values[0]), Float(values[1]), Float(values[2]), Float(values[3])
+        x_size, y_size, x_start, y_start, x_goal, y_goal = read_header file
 
         t.show_marker 'at' => [x_start / x_size, y_start / y_size],
                 'marker' => Bullet, 'scale' => 0.5
@@ -33,6 +38,66 @@ class Plotter
             x0, y0 = Float(values[0]), Float(values[1])
             x1, y1 = Float(values[3]), Float(values[4])
             t.stroke_line x0/x_size, y0/y_size, x1/x_size, y1/y_size
+        end
+    end
+
+    def dstar_costs
+        max = nil
+        t.subplot('right_margin' => 0.07) { max = cost_image }
+        t.subplot('left_margin' => 0.95,
+                  'top_margin' => 0.05,
+                  'bottom_margin' => 0.05) { gradient_bar("Cost", 0, max, t.rainbow_colormap) }
+    end
+
+    def cost_image
+        t.line_width = 0
+
+        file = StringIO.new data
+        x_size, y_size, x_start, y_start, x_goal, y_goal = read_header file
+
+        costs = Dtable.new(x_size, y_size)
+        file.each_line do |line|
+            values = line.split " "
+            x, y = Integer(values[0]), Integer(values[1])
+            costs[y, x] = Float(values[2])
+        end
+        max = costs.max
+        image = t.create_image_data(costs, 'min_value' => 0, 'max_value' => max)
+
+        t.fill_color = Wheat
+        t.fill_frame
+        t.show_image(
+                'll' => [0, 0],
+                'lr' => [1, 0],
+                'ul' => [0, 1],
+                'color_space' => t.rainbow_colormap,
+                'data' => image,
+                'w' => x_size, 'h' => y_size)
+
+        t.show_marker 'at' => [x_goal / x_size, y_goal / y_size],
+                'marker' => Bullet, 'scale' => 0.5
+        max
+    end
+
+    def gradient_bar(title, min, max, colormap)
+        xmin = 0; xmax = 1; xmid = 0.5
+        t.rescale(0.8)
+        t.xaxis_type = AXIS_LINE_ONLY
+        t.xaxis_loc  = BOTTOM
+        t.top_edge_type = AXIS_LINE_ONLY
+        t.yaxis_loc = t.ylabel_side = RIGHT
+        t.yaxis_type = AXIS_WITH_TICKS_AND_NUMERIC_LABELS
+        t.left_edge_type = AXIS_WITH_TICKS_ONLY
+        t.ylabel_shift += 0.5
+        t.yaxis_major_tick_length *= 0.6
+        t.yaxis_minor_tick_length *= 0.5
+        t.do_box_labels(nil, nil, title)
+        t.show_plot('boundaries' => [xmin, xmax, min, max]) do
+            t.axial_shading(
+                'start_point' => [xmid, min],
+                'end_point'   => [xmid, max],
+                'colormap'    => colormap
+            )
         end
     end
 end
