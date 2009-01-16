a9ea7fd0233712778ccfa71ce751f9f1a2caf8fb
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2009-01-16 12:06:24 +0100

    paper: add a reduced terrain version for faster tests

--- paper/run.sh
+++ paper/run.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-time ../build/bin/dstar terrain.tif terrain_classes.txt 76 71 out 865 285 1
-dot -Tpng out-corridors.dot > out-corridors.png
-
-okular out-corridors.png out-corridors.tif &
+time ../build/bin/dstar terrain.tif terrain_classes.txt 76 71 full 865 285 $1
+dot -Tsvg full-corridors.dot > full-corridors.svg
 
