48b0a9fa127c9db29a3a9a07d515587ed3d95915
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-02-09 18:07:37 +0100

    fix run scripts in resources/ since the executable moved

--- resources/dfki_test_track/run-small.sh
+++ resources/dfki_test_track/run-small.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-time ../../build/bin/nav terrain-small.tif terrain_classes.txt 38 37 small 440 155 $1
+time $2 ../../build/src/nav terrain-small terrain_classes.txt 38 37 small 440 155 $1
 dot -Tsvg small-corridors.dot > small-corridors.svg
 
--- resources/dfki_test_track/run.sh
+++ resources/dfki_test_track/run.sh
@@ -1,6 +1,6 @@
-#! /bin/zsh
+#! /bin/sh
 
-time ../../build/bin/nav terrain terrain_classes.txt 76 71 full 865 285 $1
+time $2 ../../build/src/nav terrain terrain_classes.txt 76 71 full 865 285 $1
 dot -Tsvg full-corridors.dot > full-corridors.svg
 dot -Tsvg full-blocked-corridors.dot > full-blocked-corridors.svg
 dot -Tsvg full-merge-corridors.dot > full-merge-corridors.svg
