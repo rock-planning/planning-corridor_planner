0c27a687945dbc1026516598f17f069485d111b2
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-09-19 17:11:56 +0200

    declare the vizkit plugin in vizkit/ruby

---
+++ viz/vizkit_plugin.rb
@@ -0,0 +1,3 @@
+Vizkit::UiLoader.register_3d_plugin('CorridorPlanViewer', 'corridor_planner', 'CorridorPlanVisualization', 'updateCorridorPlan', 'updatePlan')
+Vizkit::UiLoader.register_3d_plugin_for('CorridorPlanViewer', "/corridors/Plan_m", :updateCorridorPlan)
+
