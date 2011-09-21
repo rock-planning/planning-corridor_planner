fd90ff73cd3f95e056cf82ee8f328a4e9df25709
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-09-21 18:39:46 +0200

    vizkit: fix declaration on the Ruby side

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -1,3 +1,3 @@
-Vizkit::UiLoader.register_3d_plugin('CorridorPlanViewer', 'corridor_planner', 'CorridorPlanVisualization', 'updateCorridorPlan', 'updatePlan')
-Vizkit::UiLoader.register_3d_plugin_for('CorridorPlanViewer', "/corridors/Plan_m", :updateCorridorPlan)
+Vizkit::UiLoader.register_3d_plugin('CorridorPlanViewer', 'corridor_planner', 'CorridorPlanVisualization')
+Vizkit::UiLoader.register_3d_plugin_for('CorridorPlanViewer', "/corridors/Plan_m", :updatePlan)
 
