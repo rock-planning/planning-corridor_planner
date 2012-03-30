c4beb3d72e8d87fb37df59aa7b27cd7883eeabc7
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-03-30 19:04:32 +0200

    make the new CorridorPlanView the default, and rename CorridorPlanViewer to CorridorPlan3DPlugin

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -1,8 +1,8 @@
-Vizkit::UiLoader.register_3d_plugin('CorridorPlanViewer', 'corridor_planner', 'CorridorPlanVisualization')
-Vizkit::UiLoader.register_3d_plugin_for('CorridorPlanViewer', "/corridors/Plan_m", :updatePlan)
+Vizkit::UiLoader.register_3d_plugin('CorridorPlan3DPlugin', 'corridor_planner', 'CorridorPlanVisualization')
+Vizkit::UiLoader.register_3d_plugin_for('CorridorPlan3DPlugin', "/corridors/Plan_m", :updatePlan)
 
 require 'corridor_planner/corridor_plan_view'
 Vizkit::UiLoader.register_ruby_widget("CorridorPlanView", CorridorPlanView.method(:new))
-Vizkit::UiLoader.register_widget_for("CorridorPlanView","/corridors/Plan")
+Vizkit::UiLoader.register_default_widget_for("CorridorPlanView","/corridors/Plan")
 
 
